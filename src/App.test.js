31502a39ad1f3995355c95eb39e9f2acdf8bf619
yuriichajka <yuriichajka@gmail.com>
2021-09-03 10:46:31 +0300

    Initial commit

---
+++ src/App.test.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import axios from 'axios';
+import { render, screen, act } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import App from './App';
+
+jest.mock("axios");
+const hits = [
+    { objectID: "1", title: "Angular" },
+    { objectID: "2", title: "React" },
+];
+
+describe("App", () => {
+    it("should fetches news from an API", async () => {
+        axios.get.mockImplementationOnce(() => Promise.resolve({ data: { hits } }));
+        render(<App />);
+        userEvent.click(screen.getByRole("button"));
+        const items = await screen.findAllByRole("listitem");
+        expect(items).toHaveLength(2);
+        // Additional
+        expect(axios.get).toHaveBeenCalledTimes(1);
+        expect(axios.get).toHaveBeenCalledWith(
+            "http://hn.algolia.com/api/v1/search?query=React"
+        );
+    });
+
+    it("should fetches news from an API and reject", async () => {
+        axios.get.mockImplementationOnce(() => Promise.reject(new Error()));
+        render(<App />);
+        userEvent.click(screen.getByRole("button"));
+        const message = await screen.findByText(/Something went wrong/);
+        expect(message).toBeInTheDocument();
+    });
+
+    it("should fetches news from an API (alternative)", async () => {
+        const promise = Promise.resolve({ data: { hits } });
+        axios.get.mockImplementationOnce(() => promise);
+        render(<App />);
+        userEvent.click(screen.getByRole('button'));
+        await act(() => promise);
+        expect(screen.getAllByRole('listitem')).toHaveLength(2);
+    });
+});
